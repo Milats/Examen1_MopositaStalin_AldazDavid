7d6423eb6aefa9a5d35bae08215515018db9a589
Milats <israestan@gmail.com>
2021-12-06 18:16:54 -0500

    MopositaStalinAldazDavid 18:15

--- MopositaStalin_AldazDavid_Ejercicio2/app/src/main/java/fisei/mopositastalin_aldazdavid_ejercicio2/MainActivity.java
+++ MopositaStalin_AldazDavid_Ejercicio2/app/src/main/java/fisei/mopositastalin_aldazdavid_ejercicio2/MainActivity.java
@@ -3,12 +3,42 @@ package fisei.mopositastalin_aldazdavid_ejercicio2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public EditText number1;
+    public EditText number2;
+    public EditText pot1;
+    public EditText pot2;
+    public EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeComponents();
+
+    }
+
+
+    public void onClicRestar(View view){
+
+    }
+
+    public String Restar(String num1, String num2){
+        String result = "";
+
+        return result;
+    }
+
+    public void initializeComponents(){
+        number1 = findViewById(R.id.ma_etNumber1_sd);
+        number2 = findViewById(R.id.ma_etNumber2_sd);
+        pot1 = findViewById(R.id.ma_etNumber1_sd);
+        pot2 = findViewById(R.id.ma_etNumber2_sd);
+        result = findViewById(R.id.ma_etResultr_sd);
     }
 }
